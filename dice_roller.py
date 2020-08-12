ef094ff9a9c16eaec035a1e96ca1a1b332ba56ad
NMrocks <nishant28076@gmail.com>
2020-08-12 17:39:56 +0530

    Added dice_roller.py

---
+++ dice_roller.py
@@ -0,0 +1,19 @@
+# Super simple dice roll program
+# Enjoy!
+# Pull requests are always welcome!
+
+import random
+import tkinter as tk
+
+root=tk.Tk()
+root.title("Dice Roller")
+
+def coin_toss():
+    tk.Label(text=random.choice(("Head", "Tail"))).grid(row=1, column=1, sticky="EW")
+
+def dice_roll():
+    tk.Label(text=random.randint(1, 6)).grid(row=1, column=2, sticky="EW")
+
+tk.Button(text="Toss Coin", command=coin_toss).grid(row=2, column=1, sticky="NSEW")
+tk.Button(text="Roll Dice", command=dice_roll).grid(row=2, column=2, sticky="NSEW")
+root.mainloop()
